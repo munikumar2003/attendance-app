47cc3ea078d7a0e9fa3fcbd48fbab0ead78fe5aa
munikumar2003 <munikumarchemuru2003@gmail.com>
2026-09-16 16:09:19 +0530

    Final one

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.bkp75d1pem8"
+    "revision": "0.2jbrr42bsb8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
